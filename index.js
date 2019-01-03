7f451ba365e7d1d744ab0a31f9cc4a787a0ceda4
Jeong Jin Tak <jeongjintak7@gmail.com>
2019-01-03 16:29:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 function takeANumber(currentPos, name){
-  this.print = function(){
+  this.print = function() {
     console.log(currentPos);
   };
-  this.enqueue = function(element){
-    collection.push(element);
+  this.enqueue = function(element) {
+    currentPos.push(element);
   };
-  
+  this.dequeue = function() {
+    return currentPos.shift();
+  };
+  this.front = function() {
+    return currentPos[0];
+  };
+  this.size = function() {
+    return currentPos.length;
+  };
+  this.isEmpty = function() {
+    return (currentPos.length === 0);
+  };
   var welcomeMsg = "Welcome, " + name + ". You are number " + currentPos + " in line.";
   
   return welcomeMsg;
