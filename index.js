087db64b8b40e4d2acd5f2341d3fc54d4925c33d
Jeong Jin Tak <jeongjintak7@gmail.com>
2019-01-03 16:13:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(currentPos, name){
+  
+  var welcomeMsg = "Welcome, " + name + ". You are number  " + currentPos + " in line.";
+  
+  return welcomeMsg;
+}
+
+function nowServing(){
+  
+}
+
+function currentLine(){
+  
+}
+
